a59ef8dcb6287c7797d2e7ec3e80f43a3cfec516
Carles Pueyo <cpcastells@gmail.com>
2023-04-25 19:33:26 +0200

    Add html tags

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("Given an App component", () => {
+  describe("When rendered", () => {
+    test("Then it should show the text 'Star Wars", () => {
+      const expectedText = "Star Wars";
+
+      render(<App />);
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
