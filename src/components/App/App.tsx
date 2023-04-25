a3878f94f6e1522ba3dd0b6a0dce9638c5aaf194
Carles Pueyo <cpcastells@gmail.com>
2023-04-26 01:19:42 +0200

    Implement fetch and loadCharacters function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import { type CharacterStructure } from "../types";
+
+const apiUrl = "http://localhost:4000/characters";
+
 const App = (): JSX.Element => {
+  const [characters, setCharacters] = useState<CharacterStructure[]>([]);
+
+  useEffect(() => {
+    async () => {
+      const response = await fetch(apiUrl);
+      const charactersApi = (await response.json()) as CharacterStructure[];
+      loadCharacters(charactersApi);
+    };
+  }, []);
+
+  const loadCharacters = (characterList: CharacterStructure[]): void => {
+    setCharacters(characterList);
+  };
+
   return (
     <div className="container">
       <header className="main-header">
