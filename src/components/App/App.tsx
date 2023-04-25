fa84fd4976fe702dc2b6a68b6384db9e63448cc3
Carles Pueyo <cpcastells@gmail.com>
2023-04-26 01:46:30 +0200

    Implement mapping of characters

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { type CharacterStructure } from "../types";
+import Character from "../Character/Character";
 
 const apiUrl = "http://localhost:4000/characters";
 
@@ -7,11 +8,11 @@ const App = (): JSX.Element => {
   const [characters, setCharacters] = useState<CharacterStructure[]>([]);
 
   useEffect(() => {
-    async () => {
+    (async () => {
       const response = await fetch(apiUrl);
       const charactersApi = (await response.json()) as CharacterStructure[];
       loadCharacters(charactersApi);
-    };
+    })();
   }, []);
 
   const loadCharacters = (characterList: CharacterStructure[]): void => {
@@ -24,7 +25,13 @@ const App = (): JSX.Element => {
         <h1 className="main-title">Star Wars</h1>
       </header>
       <main className="main">
-        <ul className="characters"></ul>
+        <ul className="characters">
+          {characters.map((character) => (
+            <li key={character.id}>
+              <Character character={character} />
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
