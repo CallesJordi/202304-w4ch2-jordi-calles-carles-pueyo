c7b141da8559efce257bb886e44f61670c1c77f0
Carles Pueyo <cpcastells@gmail.com>
2023-04-26 00:45:07 +0200

    Add image url

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -5,13 +5,17 @@ interface CharacterProps {
 }
 
 const Character = ({
-  character: { name, height, mass, created },
+  character: { id, name, height, mass, created },
 }: CharacterProps): JSX.Element => {
   return (
     <>
       <div className="card">
         <h1 className="card__title">{name}</h1>
-        <img className="card__image" src="ruta-de-la-imagen.jpg" alt={name} />
+        <img
+          className="card__image"
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt={name}
+        />
         <div className="card__info">
           <span className="card__info-item">Height: {height}</span>
           <span className="card__info-item">Mass: {mass}</span>
